f8667a764c54ad397e58204e1fdc3ac374f7c54d
Lingzt <asklingtoby@gmail.com>
2017-03-14 18:06:55 -0400

    P6_1

---
+++ final/P6_1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 11:58:05 2017 @author: lingtoby
+"""
+class Person(object):     
+    def __init__(self, name):         
+        self.name = name     
+    def say(self, stuff):         
+        return self.name + ' says: ' + stuff     
+    def __str__(self):         
+        return self.name  
+
+class Lecturer(Person):     
+    def lecture(self, stuff):         
+        return 'I believe that ' + Person.say(self, stuff)  
+
+class Professor(Lecturer): 
+    def say(self, stuff): 
+        return self.name + ' says: ' + self.lecture(stuff)
+
+class ArrogantProfessor(Professor): 
+    def say(self, stuff): 
+        print('eric says: It is obvious that eric says: the sky is blue')
+        return self.name+ ' says: It is obvious that ' + Person.say(self,stuff)
+    def lecture(self, stuff):         
+        print('It is obvious that eric says: the sky is blue')
+        return 'It is obvious that ' + Person.say(self, stuff) 
+
+e = Person('eric') 
+le = Lecturer('eric') 
+pe = Professor('eric') 
+ae = ArrogantProfessor('eric')
+"""
+>>> e.say('the sky is blue')
+eric says: the sky is blue
+
+>>> le.say('the sky is blue')
+eric says: the sky is blue
+
+>>> le.lecture('the sky is blue')
+I believe that eric says: the sky is blue
+
+>>> pe.say('the sky is blue')
+eric says: I believe that eric says: the sky is blue
+
+>>> pe.lecture('the sky is blue')
+I believe that eric says: the sky is blue
+
+>>> ae.say('the sky is blue')
+eric says: It is obvious that eric says: the sky is blue
+
+>>> ae.lecture('the sky is blue')
+It is obvious that eric says: the sky is blue
+"""
