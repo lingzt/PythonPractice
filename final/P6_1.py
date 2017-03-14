5ecd7fe123af0b2666f82009cb12f3ba42be07ce
Lingzt <asklingtoby@gmail.com>
2017-03-14 18:40:18 -0400

    P6_2 and P_3

--- final/P6_1.py
+++ final/P6_1.py
@@ -17,39 +17,26 @@ class Lecturer(Person):
 
 class Professor(Lecturer): 
     def say(self, stuff): 
-        return self.name + ' says: ' + self.lecture(stuff)
+        return 'Prof. '+self.name + ' says: ' + self.lecture(stuff)
 
 class ArrogantProfessor(Professor): 
     def say(self, stuff): 
-        print('eric says: It is obvious that eric says: the sky is blue')
-        return self.name+ ' says: It is obvious that ' + Person.say(self,stuff)
+        print('eric says: It is obvious that I believe that eric says: the sky is blue')
+        return Person.say(self,self.lecture(stuff))
     def lecture(self, stuff):         
-        print('It is obvious that eric says: the sky is blue')
-        return 'It is obvious that ' + Person.say(self, stuff) 
+        #print('It is obvious that I believe that eric says: the sky is blue')
+        return 'It is obvious that '+ Lecturer.lecture(self,stuff)
 
 e = Person('eric') 
 le = Lecturer('eric') 
 pe = Professor('eric') 
 ae = ArrogantProfessor('eric')
+
+
 """
->>> e.say('the sky is blue')
-eric says: the sky is blue
-
->>> le.say('the sky is blue')
-eric says: the sky is blue
-
->>> le.lecture('the sky is blue')
-I believe that eric says: the sky is blue
-
 >>> pe.say('the sky is blue')
-eric says: I believe that eric says: the sky is blue
-
->>> pe.lecture('the sky is blue')
-I believe that eric says: the sky is blue
+Prof. eric says: I believe that eric says: the sky is blue 
 
 >>> ae.say('the sky is blue')
-eric says: It is obvious that eric says: the sky is blue
-
->>> ae.lecture('the sky is blue')
-It is obvious that eric says: the sky is blue
+Prof. eric says: It is obvious that I believe that eric says: the sky is blue 
 """
