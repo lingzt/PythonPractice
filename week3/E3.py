4c758060c40f6beb3b99363e7c45c2f31887b2b6
Lingzt <asklingtoby@gmail.com>
2017-03-16 19:32:43 -0400

    learn python list, sort, insert, append, extend, pop, reverse

---
+++ week3/E3.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 18:50:23 2017
+
+@author: lingtoby
+"""
+
+listA = [1, 4, 3, 0]
+listB = ['x', 'z', 't', 'q']
+
+
+listA.sort
+#return a function,
+
+listA.sort()
+#do not return anything, but sort the list
+
+listA.insert(0, 100)
+#do not return anything, but insert the items
+
+listA.append(7)
+#no return
+
+listA + listB
+# return [100, 0, 1, 4, 7, 'x', 'z', 't', 'q']
+'''
+allow different types in one list
+'''
+
+#listB.remove('a')
+#return error
+
+listA.extend([4, 1, 6, 3, 4])
+#return nothing
+
+listA.pop(4)
+#() is the index of the item you are popping
+
+listA.reverse()
+#return nothing
+
+
+
+
+
